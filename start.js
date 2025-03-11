45a445eeb2ec1060a6443b5673313359752b5957
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-11 00:05:42 -0700

    update

--- start.js
+++ start.js
@@ -13,7 +13,7 @@ module.exports = async (kernel) => {
           },
           path: "app",                // Edit this to customize the path to start the shell from
           message: [
-            "python gradio_server.py --profile {{args.profile}} --lora-dir ./loras {{args.mode ? args.mode : ''}} {{args.compile ? '--compile' : ''}} {{args.attention ? args.attention : ''}}",    // Edit with your custom commands
+            "python gradio_server.py --profile {{args.profile}} {{args.mode ? args.mode : ''}} {{args.compile ? '--compile' : ''}} {{args.attention ? args.attention : ''}}",    // Edit with your custom commands
           ],
           on: [{
             "event": "/http:\/\/[0-9.:]+/",   
