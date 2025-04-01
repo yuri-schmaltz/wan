55519cac2e59046b2f6a4c480872a41dc085dde1
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-01 23:49:29 +0200

    torch rocm 6.2

--- torch.js
+++ torch.js
@@ -110,7 +110,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/rocm6.0"
+        "message": "uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/rocm6.2"
       }
     },
     // linux cpu
