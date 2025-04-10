d81306126da07f8c0fbe53b53a8eb172b747ebd6
ai_anchorite <ai_anchorite@proton.me>
2025-04-10 14:05:02 +1200

    add nvidia check to install

--- install.js
+++ install.js
@@ -2,6 +2,14 @@ module.exports = {
   run: [
     // Edit this step to customize the git repository to use
     {
+     when: "{{gpu !== 'nvidia'}}",
+     method: "notify",
+     params: {
+       html: "This app requires an NVIDIA GPU."
+     }, 
+      next: null
+    },
+    {
       method: "shell.run",
       params: {
         message: [
