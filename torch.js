ec6c55bf239dd3c608c3fbb42e7398a9403a8552
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-23 16:07:59 -0400

    pytorch2.7 stable

--- torch.js
+++ torch.js
@@ -8,7 +8,7 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          'uv pip install --pre "torch<2.8.0" torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128',
+          'uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128',
           "uv pip install triton-windows",
           "uv pip install https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl", 
 //          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp310-cp310-win_amd64.whl",
@@ -25,7 +25,7 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128",
+          'uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128',
           "uv pip install git+https://github.com/thu-ml/SageAttention.git",
 //          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp310-cp310-win_amd64.whl",
 //          "uv pip install https://github.com/deepbeepmeep/SageAttention/raw/refs/heads/main/releases/sageattention-2.1.0-cp310-cp310-win_amd64.whl"
